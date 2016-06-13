96692115df4cc936f32836c1138e59c518f6a731
vermosen <vermosen.jm@free.fr>
2016-06-12 21:05:18 -0400

    initial commit

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.Service1Client myService = new ServiceReference1.Service1Client();
+            MessageBox.Show(myService.GetData(123), "my service");
+            myService.Close();
+        }
+    }
+}
---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using testWcf;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        internal static ServiceHost myServiceHost = null;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if (myServiceHost != null)
+            {
+                myServiceHost.Close();
+            }
+            myServiceHost = new ServiceHost(typeof(testWcf.Service1));
+            myServiceHost.Open();
+        }
+
+
+        protected override void OnStop()
+        {
+            if (myServiceHost != null)
+            {
+                myServiceHost.Close();
+                myServiceHost = null;
+            }
+        }
+
+    }
+}
